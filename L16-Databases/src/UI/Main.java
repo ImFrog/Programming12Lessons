010df86b7dc194f3bc0624525bf32850453a2cff
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-05-16 09:59:46 -0700

    NETWORKING

---
+++ L16-Databases/src/UI/Main.java
@@ -0,0 +1,9 @@
+package UI;
+
+import Database.DatabaseHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseHandler handler = new DatabaseHandler();
+    }
+}
--- L5-GUI/src/sample/Controller.java
+++ L5-GUI/src/sample/Controller.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
